9a331579e8471e6482ff13b939997e94f6119a15
elcabong <mbarone000@gmail.com>
2016-05-02 21:14:20 -0700

    settings area continue with templates setup

--- js/controllers/settingsCtrl.js
+++ js/controllers/settingsCtrl.js
@@ -1,10 +1,65 @@
 'use strict';
 
 app.settingsController('settingsCtrl', ['$scope','$timeout','$http','inform','Idle', function ($scope, $timeout, $http, inform, Idle){
+	$scope.userdata = [];
+	$scope.userdata.username=sessionStorage.getItem('username');
+	$scope.userdata.userid=sessionStorage.getItem('userid');
+	$scope.userdata.mobile=sessionStorage.getItem('mobile');
 
 
 
 
+    $scope.tabs = [{
+            title: 'One',
+            url: './partials/tpl/settings1.tpl.html'
+        }, {
+            title: 'Two',
+            url: './partials/tpl/settings2.tpl.html'
+        }, {
+            title: 'Three',
+            url: './partials/tpl/settings3.tpl.html'
+    }];
+
+    $scope.currentTab = './partials/tpl/settings1.tpl.html';
+
+    $scope.onClickTab = function (tab) {
+        $scope.currentTab = tab.url;
+    }
+    
+    $scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
+
+
+
+	
+	
+	$scope.logout=function(){
+		loginService.logout();
+	};
+
+	$scope.testmessage = function($scope) {
+		inform.add('test');
+		inform.add('Default', {
+		  ttl: 120000, type: 'default'
+		});
+		inform.add('Primary with long text string to se asdhafsdifjaskjdf a skdjf laskdjflkajsdf alksdj flkasjdf', {
+		  ttl: 120000, type: 'primary'
+		});
+		inform.add('Info', {
+		  ttl: 120000, type: 'info'
+		});
+		inform.add('Success', {
+		  ttl: 120000, type: 'success'
+		});
+		inform.add('Warning', {
+		  ttl: 120000, type: 'warning'
+		});
+		inform.add('Danger', {
+		  ttl: 120000, type: 'danger'
+		});
+	};	
+	
 }])
 
 
