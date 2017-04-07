de4a0b9140828465b7e65975ad8029c29cc36c86
mike <mbarone000@gmail.com>
2017-04-06 18:08:21 -0700

    updates to settings navigation page added keylength filter to count length of objects

--- js/services/filterService.js
+++ js/services/filterService.js
@@ -58,3 +58,12 @@ app.filter('positive', function() {
 		return Math.abs(input);
 	};
 });
+
+app.filter('keylength', function(){
+  return function(input){
+    if(!angular.isObject(input)){
+      throw Error("Usage of non-objects with keylength filter!!")
+    }
+    return Object.keys(input).length;
+  }
+});
